983249386b7732f7cc49615356af9eda02777bde
alezgutmann <supersalat090505@gmail.com>
2025-05-30 12:46:35 +0200

    hühner können nun geladen werden

--- src/huhn_sim/config.java
+++ src/huhn_sim/config.java
@@ -9,11 +9,11 @@ public class config {
 	public static boolean loadFromOBJFile = true;
 	public static String fragShaderPath = "src/shader/staub_frag_shader.fs";
 	public static double rainbowMultiplier = 9.0; // um welchen Faktor die Hühner im Rainbow mode schneller sein sollen 
-	public static String OBJFilePath = "resources/maennchen.obj";
+	public static String OBJFilePath = "resources/huhn_weiss.obj";
 	public static boolean renderFromQUADS = false; //funktioniert momentan noch nicht naja egal
-	public static double HühnerSize = 10.0;
+	public static double HühnerSize = 10000.0;
 	public static double zOFFSET = 0; //wie tief huhn im boden "steckt" bzw heraus kommt
-	public static int AGENTEN_ANZAHL = 2;
+	public static int AGENTEN_ANZAHL = 20;
 	public static int AGENTEN_MASS = 1;
 	public static int AGENTEN_MAX_SPEED = 100;
 	public static int AGENTEN_MAX_TURN_RATE = 15;
@@ -26,7 +26,7 @@ public class config {
 	public static double KOERNER_FORCE = 5; // soll später die FORCE die die hühner zu körnern verspüren darstellen
 	public static double HOMING_FORCE = 30;
 	public static double SCHWARZES_SCHAF_FORCE = 100;
-	public static double SICHTWEITE = 2000; // ab wann die hühner die körner sehen
-	public static double SCHNABELWEITE = 20; // distanz die ein huhn zum korn haben muss um es zu schnabulieren
+	public static double SICHTWEITE = 200; // ab wann die hühner die körner sehen
+	public static double SCHNABELWEITE = 10; // distanz die ein huhn zum korn haben muss um es zu schnabulieren
 	public static double KORN_SPREAD = 50; // wie weit die körner "gestreut" werden
 }
